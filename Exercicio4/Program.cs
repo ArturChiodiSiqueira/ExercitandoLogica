baa9c5afb509323b04e38c8ba9e1e849e532adcb
ArturChiodiSiqueira <arturchiodi@outlook.com>
2022-06-17 15:05:25 -0300

    add project

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //para fazer.............
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
