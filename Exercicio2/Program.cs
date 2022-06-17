e6e77dafddf3d0ea57a0d0089d0c6db0b4f3f5db
ArturChiodiSiqueira <arturchiodi@outlook.com>
2022-06-17 11:38:46 -0300

    add project

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.Write("Que horas são? ");
+            double horas = double.Parse(Console.ReadLine());
+
+            if (horas >= 0.0 && horas <= 12.0)
+            {
+                Console.WriteLine("Bom dia, " + nome + ". Seja bem vindo(a)!");
+            }
+            else if (horas > 12.0 && horas <= 18.0)
+            {
+                Console.WriteLine("Boa tarde, " + nome + ". Seja bem vindo(a)!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite, " + nome + ". Seja bem vindo(a)!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
